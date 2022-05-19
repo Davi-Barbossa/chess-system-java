175311f99745d2486a391c433348ed69c128d0eb
Davi Barbosa <bdavi186@gmail.com>
2022-05-18 21:13:44 -0300

    Inclusão de métodos para validaçao de jogadas e peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -23,13 +24,51 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+    	Position position = sourcePosition.toPosition();
+    	validateSourcePosition(position);
+    	return board.piece(position).possibleMoves();
+    	
+    }
+	
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		validateTargetPosition(source, target);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	
+	private Piece makeMove(Position source, Position target){
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("Não existe peça na posição indicada.");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existem movimentos possíveis para a peça escolhida!");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peça escolhida não pode ir para o destino desejado(movimento inválido para a peça).");
+		}
+	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 	
 	private void initialSetup() {
-		   placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		    placeNewPiece('c', 2, new Rook(board, Color.WHITE));
 	        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
 	        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
 	        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
